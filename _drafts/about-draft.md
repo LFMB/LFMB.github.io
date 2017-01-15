fe8568839e8360a43e50290202d402e61f149456
LFMB <lfblom@rcn.com>
2017-01-15 16:38:38 -0500

    started work on the showing the distribution of gdp per captia breakdown for Q1 2015

--- _drafts/about-draft.md
+++ _drafts/about-draft.md
@@ -7,11 +7,15 @@ order: 2
 
 # About
 
+Downeast Markets is interested in the intersection between finance, government and tech. This intersection contains tremendous opportunity in expanding the ability to make intelligent decisions for everyone in our society. We are looking to create and share content that will add to this expansion. 
+
 This site years ago talked with glee about the coming power of: MOOCs, fast and ubiquitous internet, STEM fields, realtime society spanning granular data and innovative professionals who can skillfully weave the creative with the technical. Hello past, welcome to the future. Yes, we have entered the hypothesized stage where all these factors can, and are powerful forces in our economy and overall society. They are changing how patients receive medical care and advice, how people date and find jobs to even changing how we engage with religion. Downeast Markets in particular is interested in the intersection between these forces and two sectors: finance and government. 
 
 The reason for this interest is twofold. First is due to the scale and pervasive nature of both. Every bit of a person’s life is touched by government and finance. It starts with which timezone you wake up in, spans to influencing almost all aspects of marriage and ends with how your life works gets transferred at your death.  Second deals with the the belief that both are ripe for massive overhauls. Currently both sectors routinely deal with information asymmetry, employ people from similar backgrounds as their professional peers and rarely are penalized for suboptimal outcomes especially if they mirror the population mean. This is all in spite of facing “fierce” and routine performance reviews. After considering the implications of today’s environment and the above characteristics, it is easy to see a coming wave that will destroy many previously held functions, demand drastically new skillsets, create new jobs and greatly empower previously less engaged stakeholders at relatively little cost.
 
-Downeast Markets sees a relatively soon future that gives “dumb” money the ability to be “smart”.
+
+
+
 
 
 
--- scripts/test-gdp-breakdown.js
+++ scripts/test-gdp-breakdown.js
@@ -1,4 +1,4 @@
-
+/*
 var containerWidth = document.querySelector('.usa-map-container').innerWidth,
     width, 
     height;
@@ -7,28 +7,21 @@ var containerWidth = document.querySelector('.usa-map-container').innerWidth,
 
     height = width / 2; 
 
-
-
-
 var projection = d3.geo.albersUsa()
     .translate([width / 2, height / 2])
-    .scale([1000]); // scale things down so see entire US
+    .scale([1000]); 
 
 var path = d3.geo.path().projection(projection);
 
-// Define linear scale for output
 var color = d3.scale.linear()
     .range(["rgb(255,0,0)", "rgb(255,255,255)", "rgb(0,128,0)"]);
 
 
-//Create SVG element and append map to the SVG
 var usaMap = d3.select("#usa-map")
     .append("svg")
     .attr('class', 'state-of-union-map')
     .attr("width", width)
     .attr("height", height);
-
-// Append Div for tooltip to SVG
 
 var tooltip = d3.select("#usa-map")
     .append("svg")
@@ -67,27 +60,8 @@ d3.json("../data/raw-and-converted-per-capita-gdp-data.json", function(GDPbreakd
 
     
     nationalPerCapita.text('national per capita: ' + GDPbreakdowns.nation["2015"]["converted"][0]["Per capita GDP"]);
-
-
-    // Load GeoJSON data and merge with states data
     
     d3.json("../data/us-states.json", function(geoData) {
-
-        /*
-        let stateName = "",
-            stateConvertedGDP = ""
-            stateRawDif = 0,
-            stateConvertedDiff = "",
-            stateConvertedPercentDiff = "";
-
-        let q1GDP2015 = GDPbreakdowns.states.map(state => ({
-            gdpConvertedBreakdown: state["2015"]["converted"][0]["Per capita GDP"],
-            gdpRawDif: state["2015"]["raw"][0]["Actual Diff From nation"],
-            gdpConvertedDif: state["2015"]["converted"][0]["Actual Diff From nation"],
-            gdpConvertedPercentDiff: state["2015"]["converted"][0]["% Diff from nation"],
-            state: state.state
-        }));
-        */
 
         function filterByPropertiesName(obj) {
             if (obj.properties.name === stateName) {
@@ -125,7 +99,6 @@ d3.json("../data/raw-and-converted-per-capita-gdp-data.json", function(GDPbreakd
             geoData.features.filter(filterByPropertiesName);
         }
         
-        // Bind the data to the SVG and create one path per GeoJSON feature
         usaMap.selectAll("path")
             .data(geoData.features)
             .enter()
@@ -134,13 +107,10 @@ d3.json("../data/raw-and-converted-per-capita-gdp-data.json", function(GDPbreakd
             .style("stroke", "#000")
             .style("stroke-width", "1")
             .style("fill", function(d) {
-                // Get data value
                 var value = d.properties["rawDiff"];
                 if (value) {
-                    //If value exists…
                     return color(value);
                 } else {
-                    //If value is undefined…
                    return "rgb(213,222,217)";
                 }              
             })
@@ -251,9 +221,6 @@ function buildStateOfUnion(year, quarter){
 
         
         nationalPerCapita.text('national per capita: ' + GDPbreakdowns.nation[year]["converted"][quarter]["Per capita GDP"]);
-
-
-        // Load GeoJSON data and merge with states data
         
         d3.json("../data/us-states.json", function(geoData) {
 
@@ -300,13 +267,10 @@ function buildStateOfUnion(year, quarter){
                     .data(geoData.features)
                     .attr("d", path)
                     .style("fill", function(d) {
-                        // Get data value
                         var value = d.properties["rawDiff"];
                         if (value) {
-                            //If value exists…
                             return color(value);
                         } else {
-                            //If value is undefined…
                            return "rgb(213,222,217)";
                         }              
                     })
@@ -381,6 +345,149 @@ function buildStateOfUnion(year, quarter){
     });
 };
 
+*/
+/*
+d3.json("../data/raw-and-converted-per-capita-gdp-data.json", function(GDPbreakdowns) {
+
+    
+    var maxPerCapita = d3.max(GDPbreakdowns.states, function(d) {
+        return d[2015]["raw"][0]["Actual Diff From nation"];
+    });
+    var minPerCapita = d3.min(GDPbreakdowns.states, function(d) {
+        return d[2015]["raw"][0]["Actual Diff From nation"];
+    });
+
+    var histogramRange = GDPbreakdowns.states.length;
+    
+    // Generate a log-normal distribution with a median of 30 minutes.
+    // var values = d3.range(histogramRange).map(d3.random.logNormal(Math.log(30), .4));
+    var values = d3.range(histogramRange).map(GDPbreakdowns.states, function(d) {
+        return d[2015]["raw"][0]["Actual Diff From nation"];
+    });
+
+    // Formatters for counts and times (converting numbers to Dates).
+    var formatCount = d3.format(",.0f"),
+        formatTime = d3.time.format("%M"),
+        formatMinutes = function(d) { return formatTime(new Date(2012, 0, 1, 0, d)); };
+
+    var margin = {top: 10, right: 30, bottom: 30, left: 30},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+        .domain([0, 120])
+        .range([0, width]);
+
+    // Generate a histogram using twenty uniformly-spaced bins.
+    var data = d3.layout.histogram()
+        .bins(x.ticks(20))
+        (values);
+
+    var y = d3.scale.linear()
+        .domain([0, d3.max(data, function(d) { return d.y; })])
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .tickFormat(formatMinutes);
+
+    var svg = d3.select("#state-of-union-distribution").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var bar = svg.selectAll(".bar")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+    bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(data[0].dx) - 1)
+        .attr("height", function(d) { return height - y(d.y); });
+
+    bar.append("text")
+        .attr("dy", ".75em")
+        .attr("y", 6)
+        .attr("x", x(data[0].dx) / 2)
+        .attr("text-anchor", "middle")
+        .text(function(d) { return formatCount(d.y); });
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+});
+
+*/
+
+d3.json("../data/raw-and-converted-per-capita-gdp-data.json", function(GDPbreakdowns) {
+
+var histogramRange = GDPbreakdowns.states.length;
+
+var histogramData = d3.range(histogramRange).map(d3.randomBates(10));
+
+var formatCount = d3.format(",.0f");
+
+var maxPerCapitaDiff = d3.max(GDPbreakdowns.states, function(d) {
+    return d[2015]["raw"][0]["Actual Diff From nation"];
+});
+var minPerCapitaDiff = d3.min(GDPbreakdowns.states, function(d) {
+    return d[2015]["raw"][0]["Actual Diff From nation"];
+});
 
 
 
+var maxPerCapita = d3.max(GDPbreakdowns.states, function(d) {
+    return d[2015]["raw"][0]["Per capita GDP"];
+});
+var minPerCapita = d3.min(GDPbreakdowns.states, function(d) {
+    return d[2015]["raw"][0]["Per capita GDP"];
+});
+
+var svg = d3.select("#state-of-union-distribution"),
+    margin = {top: 10, right: 30, bottom: 30, left: 30},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scaleLinear()
+    .rangeRound([0, width])
+    .domain([minPerCapita, maxPerCapita]);
+
+var bins = d3.histogram()
+    .domain(x.domain())
+    .thresholds(x.ticks(20))
+    (histogramData);
+
+var y = d3.scaleLinear()
+    .domain([0, d3.max(bins, function(d) { return d.length; })])
+    .range([height, 0]);
+
+var bar = g.selectAll(".bar")
+  .data(bins)
+  .enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+
+bar.append("rect")
+    .attr("x", 1)
+    .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+    .attr("height", function(d) { return height - y(d.length); });
+
+bar.append("text")
+    .attr("dy", ".75em")
+    .attr("y", 6)
+    .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return formatCount(d.length); });
+
+g.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+});
